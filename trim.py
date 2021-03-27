7d166fa9b0281c7c883cba030af5668197f2280c
Krithin Sitaram <krithin@gmail.com>
2021-03-27 23:31:11 +0800

    Fix rotations and switch to an affine transform for image processing.
    We sort the points consistently between the input and output ends of the
    affine transform to make sure we retain the orientation of the source
    picture. Using an affine trainsform is also conceptually cleaner, since
    we don't need the extra degree of freedom the former perspective
    transform gave us.

--- trim.py
+++ trim.py
@@ -6,7 +6,7 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description="Trim and deskew an image.")
 parser.add_argument("--quiet", action="store_true", default=False, help="Don't prompt for a keypress before saving.")
-parser.add_argument("--process-scale", type=int, default=1, help="Scale intermediate processing images down by this integer factor for speed.")
+parser.add_argument("--process-scale", type=int, default=1, help="Scale intermediate processing images down by this integer factor to help previews fit on screen.")
 parser.add_argument("input_filename")
 parser.add_argument("output_filename")
 args = parser.parse_args()
@@ -17,7 +17,11 @@ if img is None:
 
 scale = args.process_scale
 if scale != 1:
-    resized = cv.resize(img, (int(img.shape[1] / scale), int(img.shape[0] / scale)), interpolation = cv.INTER_AREA)
+    resized = cv.resize(
+        img,
+        (int(img.shape[1] / scale), int(img.shape[0] / scale)),
+        interpolation = cv.INTER_AREA
+    )
 else:
     resized = img
 if not args.quiet:
@@ -32,10 +36,10 @@ if not args.quiet:
 # Draw contours
 contours, hierarchy = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-for i in range(len(contours)):
-    color = (random.randint(0,256), random.randint(0,256), random.randint(0,256))
-    cv.drawContours(drawing, contours, i, color)
 if not args.quiet:
+    for i in range(len(contours)):
+        color = (random.randint(0,256), random.randint(0,256), random.randint(0,256))
+        cv.drawContours(drawing, contours, i, color)
     cv.imshow("Contours", drawing)
     k = cv.waitKey(0)
 
@@ -45,38 +49,40 @@ real_contours = [c for c in contours if cv.arcLength(c, False) > 200.0 / scale]
 hull = cv.convexHull(
     np.array([point for contour in real_contours for point in contour])
 )
-color = (random.randint(0,256), random.randint(0,256), random.randint(0,256))
-cv.drawContours(drawing, [hull], -1, color)
 if not args.quiet:
+    color = (random.randint(0,256), random.randint(0,256), random.randint(0,256))
+    cv.drawContours(drawing, [hull], -1, color)
     cv.imshow("Contours", drawing)
     k = cv.waitKey(0)
 
+# Find the smallest (rotated) rectangle that bounds that convex hull
 rect = cv.minAreaRect(hull)
 print(args.input_filename, rect)
-box = np.int0(cv.boxPoints(rect))
-cv.drawContours(drawing, [box], -1, (255,255,255))
+corners = cv.boxPoints(rect)
 if not args.quiet:
+    cv.drawContours(drawing, [np.int0(corners)], -1, (255,255,255))
     cv.imshow("Contours", drawing)
     k = cv.waitKey(0)
 
-# get width and height of the detected rectangle
-width = int(rect[1][0])
-height = int(rect[1][1])
+# Identify points in the same order in the minimum bounding rectangle (src) and
+# the output image boundary (dst) to build an affine transform between them.
+corners = sorted(corners, key=lambda p: p[0] + p[1])
+top_left = corners[0]
+top_right, bottom_left = sorted([corners[1], corners[2]], key=lambda p: p[1])
+src_points = np.array([
+    top_left, top_right, bottom_left
+]).astype(np.float32) * scale
 
-src_pts = box.astype("float32") * scale
-# coordinate of the points in box points after the rectangle has been
-# straightened
-dst_pts = np.array([[0, height * scale -1],
-                    [0, 0],
-                    [width * scale -1, 0],
-                    [width * scale -1, height * scale -1]], dtype="float32")
+width, height = np.int0(rect[1]) * scale
+rotation_angle = rect[2]
+if rotation_angle > 80:
+    width, height = height, width
+dst_points = np.array([
+    [0, 0], [width-1, 0], [0, height-1],
+]).astype(np.float32)
 
-# the perspective transformation matrix
-M = cv.getPerspectiveTransform(src_pts, dst_pts)
+M = cv.getAffineTransform(src_points, dst_points)
 
-# directly warp the original image to get the straightened rectangle
-warped = cv.warpPerspective(img, M, (width * scale, height * scale))
-
+# Run the transform, rotating and cropping the input image.
+warped = cv.warpAffine(img, M, (width, height))
 cv.imwrite(args.output_filename, warped)
-#cv.imshow("Warped", warped)
-#cv.waitKey(0)
